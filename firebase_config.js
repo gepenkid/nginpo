efb069903837be059d9698ceb7fac1129604851e
Gepenk ID <48324642+gepenkid@users.noreply.github.com>
2025-02-14 05:50:17 +0700

    Create firebase_config.js

---
+++ firebase_config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAusoCTN7Z2YFM_jTk55Dnauc9zrPt6U_c",
+  authDomain: "nginpo.firebaseapp.com",
+  databaseURL: "https://nginpo-default-rtdb.firebaseio.com",
+  projectId: "nginpo",
+  storageBucket: "nginpo.firebasestorage.app",
+  messagingSenderId: "608334338532",
+  appId: "1:608334338532:web:6ba559f1161827189f8ff1",
+  measurementId: "G-TS99E8SKGJ"
+};
